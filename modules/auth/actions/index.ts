69e0f288b22f0bea86eee684a86b963d7c3253a5
kishanghosh090 <kishanghosh090@gamil.com>
2026-01-20 19:40:54 +0530

    fix prisma bug

---
+++ modules/auth/actions/index.ts
@@ -0,0 +1,44 @@
+"use server";
+import { db } from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+export const onBoardUser = async () => {
+  try {
+    const user = await currentUser();
+    if (!user) {
+      return {
+        success: false,
+        message: "User not found",
+      };
+    }
+    const { id, firstName, lastName, emailAddresses, imageUrl } = user;
+    const newUser = await db.user.upsert({
+      where: {
+        clerkId: id,
+      },
+      update: {
+        firstName: firstName || null,
+        lastName: lastName || null,
+        email: emailAddresses[0]?.emailAddress || "",
+        imageUrl: imageUrl || "",
+      },
+      create: {
+        clerkId: id,
+        firstName: firstName || null,
+        lastName: lastName || null,
+        email: emailAddresses[0]?.emailAddress || "",
+        imageUrl: imageUrl || "",
+      },
+    });
+    return {
+      success: true,
+      message: "User onboarded successfully",
+      user: newUser,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: "Error onboarding user",
+      error,
+    };
+  }
+};
